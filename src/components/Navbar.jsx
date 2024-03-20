41e9273b947fb7689b9fe6702914a90607f1693c
sabarish <vbsabarishsriram@gmail.com>
2024-03-20 23:46:33 +0530

    minor changes 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,10 @@ import { IoMenu } from "react-icons/io5";
 function Navbar() {
   return (
     <div className='flex bg-white w-full fixed z-10 top-0 items-center  py-4 px-4'>
-        <div className='flex justify-center items-center'>
+        <div className='flex items-center w-full'>
             <img src={Icon} alt="" className='w-9' />
-            <p className=' ml-2 font-bold text-base	'>humankind</p>
-            <div className='ml-6 hidden md:flex justify-center items-center'>
+            <p className=' font-bold text-base	'>humankind</p>
+            <div className='ml-6  hidden md:flex justify-center items-center'>
               <p className='font-bold ml-[400px]'>Wrestle</p>
               <p className='font-bold ml-[25px]'>Collectibles</p>
               <p className='font-bold ml-[25px]'>Shop</p>
