697d89d732c98f5aebe015ff171a9bc6838bc21b
sabarish <vbsabarishsriram@gmail.com>
2024-03-22 22:41:38 +0530

    navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,20 +5,18 @@ import { IoMenu } from "react-icons/io5";
 
 function Navbar() {
   return (
-    <div className='flex bg-white w-full fixed z-10 top-0 items-center  py-4 px-4'>
-        <div className='flex items-center w-full'>
+    <div className='fixed top-0 left-0 w-full ml-[20px]  hidden md:flex justify-between h-[50px] bg-white items-center'>
             <img src={Icon} alt="" className='w-9' />
-            <p className=' font-bold text-base	'>humankind</p>
-            <div className='ml-[20px]  hidden md:flex justify-center items-center'>
-              <p className='font-bold ml-[400px]'>Wrestle</p>
-              <p className='font-bold ml-[25px]'>Collectibles</p>
-              <p className='font-bold ml-[25px]'>Shop</p>
-              <p className='font-bold ml-[25px]'>Community</p>
-              <p className='font-bold ml-[25px]'>Subscribe</p>
-              <button className='bg-black text-white w-32 py-2 ml-3 font-bold rounded-full'>Play Now! 💀</button>
+            <p className=' font-bold text-base ml-2 w-full md:text-lg	'>humankind</p>
+
+              <p className='font-bold ml-1 px-3'>Wrestle</p>
+              <p className='font-bold px-3'>Collectibles</p>
+              <p className='font-bold px-3'>Shop</p>
+              <p className='font-bold px-3'>Community</p>
+              <p className='font-bold px-3'>Subscribe</p>
+              <button className='bg-black text-white w-[325px] py-2 mr-8 font-bold rounded-full'>Play Now! 💀</button>
               
-            </div>
-        </div>
+           
         <IoMenu size={40} className='fixed md:hidden right-8' /> 
     </div>
     
